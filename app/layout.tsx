309e62728eeb8eac3ed1dea95f85270a9a661952
chenkarfei <chenkarfei@gmail.com>
2026-04-21 14:48:39 +0800

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css'; // Global styles
 
 export const metadata: Metadata = {
-  title: 'My Google AI Studio App',
-  description: 'My Google AI Studio App',
+  title: 'Yolide Invoice Generator',
+  description: 'Prossional Malaysia Invoice & Receipt Generator with SST support',
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
